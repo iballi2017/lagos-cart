69368b43a36661e69bdaa65cde2fd6d04a5ffae3
iballi2017 <iballi201@gmail.com>
2023-09-01 10:46:51 +0100

    subscription plan page design implementation

--- src/index.php
+++ src/index.php
@@ -12,61 +12,63 @@
 </head>
 
 <body>
-    <header>
-        <div class="logo-wrapper">
-            <img src="./assets/images/brand-logo.svg" alt="">
-        </div>
-    </header>
+    <div class="subscription-plans">
+        <header class="my-4">
+            <div class="logo-wrapper mx-auto">
+                <img src="./assets/images/brand-logo.svg" alt="">
+            </div>
+        </header>
 
-    <main>
-        <div class="hero">
-            <h1>Elevate your shopping experience with our exclusive Prime Subscription! </h1>
-        </div>
+        <main>
+            <div class="hero">
+                <h1>Elevate your shopping experience with our exclusive Prime Subscription! </h1>
+            </div>
 
-        <ul class="plan-options">
-            <li class="plan">
-                <div class="icon">
-                    <img src="./assets/icons/icon-monthly.svg" alt="">
-                </div>
-                <h2>Monthly Plan</h2>
-                <p>N20,000</p>
-                <ul class="services">
-                    <li>Unlock endless possibilities with unlimited deliveries</li>
-                    <li>Complimentary health check-up at Mainland Clinics</li>
-                </ul>
-                <input type="button" value="Buy Plan" />
-            </li>
-            <li class="plan">
-                <div class="icon">
-                    <img src="./assets/icons/icon-quarterly.svg" alt="">
-                </div>
-                <h2>Quarterly Plan</h2>
-                <p>N45,000</p>
-                <ul class="services">
-                    <li>Unlock endless possibilities with unlimited deliveries</li>
-                    <li>Complimentary health check-up at Mainland Clinics</li>
-                    <li>Solar audit for your home or office by Mainland Solar</li>
-                </ul>
-                <input type="button" value="Buy Plan" />
-            </li>
-            <li class="plan">
-                <div class="icon">
-                    <img src="./assets/icons/icon-yearly.svg" alt="">
-                </div>
-                <h2>Yearly Plan</h2>
-                <p>N120,000</p>
-                <ul class="services">
-                    <li>Unlock endless possibilities with unlimited deliveries</li>
-                    <li>Complimentary health check-up at Mainland Clinics</li>
-                    <li>Solar audit for your home or office by Mainland Solar</li>
-                    <li>3 hour use of a VanLagos cargo or passenger can (within Lagos only)</li>
-                </ul>
-                <input type="button" value="Buy Plan" />
-            </li>
-        </ul>
+            <ul class="plan-options">
+                <li class="plan">
+                    <div class="icon">
+                        <img src="./assets/icons/icon-monthly.svg" alt="">
+                    </div>
+                    <h2>Monthly Plan</h2>
+                    <p>N20,000</p>
+                    <ul class="services">
+                        <li>Unlock endless possibilities with unlimited deliveries</li>
+                        <li>Complimentary health check-up at Mainland Clinics</li>
+                    </ul>
+                    <input type="button" value="Buy Plan" />
+                </li>
+                <li class="plan">
+                    <div class="icon">
+                        <img src="./assets/icons/icon-quarterly.svg" alt="">
+                    </div>
+                    <h2>Quarterly Plan</h2>
+                    <p>N45,000</p>
+                    <ul class="services">
+                        <li>Unlock endless possibilities with unlimited deliveries</li>
+                        <li>Complimentary health check-up at Mainland Clinics</li>
+                        <li>Solar audit for your home or office by Mainland Solar</li>
+                    </ul>
+                    <input type="button" value="Buy Plan" />
+                </li>
+                <li class="plan">
+                    <div class="icon">
+                        <img src="./assets/icons/icon-yearly.svg" alt="">
+                    </div>
+                    <h2>Yearly Plan</h2>
+                    <p>N120,000</p>
+                    <ul class="services">
+                        <li>Unlock endless possibilities with unlimited deliveries</li>
+                        <li>Complimentary health check-up at Mainland Clinics</li>
+                        <li>Solar audit for your home or office by Mainland Solar</li>
+                        <li>3 hour use of a VanLagos cargo or passenger can (within Lagos only)</li>
+                    </ul>
+                    <input type="button" value="Buy Plan" />
+                </li>
+            </ul>
 
-        <aside>Don't miss out on these extraordinary benefits. Join Prime today and redefine your shopping journey!</aside>
-    </main>
+            <aside>Don't miss out on these extraordinary benefits. Join Prime today and redefine your shopping journey!</aside>
+        </main>
+    </div>
 </body>
 
 </html>
