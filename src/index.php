9127d9e46e10cc4f25978036c70ab8a8db3d9547
iballi2017 <iballi201@gmail.com>
2023-09-01 10:37:40 +0100

    subscription plan page design implementation

---
+++ src/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lagos Cart | Plan options</title>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./stylesheets/main.css">
+    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <link rel="stylesheet" href="../dist/output.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo-wrapper">
+            <img src="./assets/images/brand-logo.svg" alt="">
+        </div>
+    </header>
+
+    <main>
+        <div class="hero">
+            <h1>Elevate your shopping experience with our exclusive Prime Subscription! </h1>
+        </div>
+
+        <ul class="plan-options">
+            <li class="plan">
+                <div class="icon">
+                    <img src="./assets/icons/icon-monthly.svg" alt="">
+                </div>
+                <h2>Monthly Plan</h2>
+                <p>N20,000</p>
+                <ul class="services">
+                    <li>Unlock endless possibilities with unlimited deliveries</li>
+                    <li>Complimentary health check-up at Mainland Clinics</li>
+                </ul>
+                <input type="button" value="Buy Plan" />
+            </li>
+            <li class="plan">
+                <div class="icon">
+                    <img src="./assets/icons/icon-quarterly.svg" alt="">
+                </div>
+                <h2>Quarterly Plan</h2>
+                <p>N45,000</p>
+                <ul class="services">
+                    <li>Unlock endless possibilities with unlimited deliveries</li>
+                    <li>Complimentary health check-up at Mainland Clinics</li>
+                    <li>Solar audit for your home or office by Mainland Solar</li>
+                </ul>
+                <input type="button" value="Buy Plan" />
+            </li>
+            <li class="plan">
+                <div class="icon">
+                    <img src="./assets/icons/icon-yearly.svg" alt="">
+                </div>
+                <h2>Yearly Plan</h2>
+                <p>N120,000</p>
+                <ul class="services">
+                    <li>Unlock endless possibilities with unlimited deliveries</li>
+                    <li>Complimentary health check-up at Mainland Clinics</li>
+                    <li>Solar audit for your home or office by Mainland Solar</li>
+                    <li>3 hour use of a VanLagos cargo or passenger can (within Lagos only)</li>
+                </ul>
+                <input type="button" value="Buy Plan" />
+            </li>
+        </ul>
+
+        <aside>Don't miss out on these extraordinary benefits. Join Prime today and redefine your shopping journey!</aside>
+    </main>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css,php}"],
+  theme: {
+    colors: {
+      // Configure your color palette here
+      // transparent: 'transparent',
+      // current: 'currentColor',
+      // 'white': '#ffffff',
+      // 'primary': '#FFBB37',
+      // 'dark': '#333333 !important',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
